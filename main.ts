f5b2c6eaf1f378a1e2316fbd87b643216c2b00e2
hoauzs <hoauzs@gmail.com>
2026-09-24 19:47:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
-let timer_to = 0
 input.onButtonPressed(Button.AB, function () {
     timer_to = randint(5, 10)
     basic.showIcon(IconNames.Chessboard)
     while (timer_to > 0) {
         timer_to += -1
         basic.pause(1000)
+        led.toggle(2, 2)
     }
     basic.showIcon(IconNames.Sad)
 })
+let timer_to = 0
+basic.showString("Hot potato")
