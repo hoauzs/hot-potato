810b74212749c3d2ef578234f470d0399a78e292
hoauzs <hoauzs@gmail.com>
2026-09-24 19:43:55 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let timer_to = 0
 input.onButtonPressed(Button.AB, function () {
-    timer_to = randint(5, 15)
+    timer_to = randint(5, 10)
     basic.showIcon(IconNames.Chessboard)
     while (timer_to > 0) {
         timer_to += -1
